89924c5d4b49ffcefce6157f4c022d5b0c49c777
sainsw <s.ainsworth@me.com>
2024-02-24 20:27:48 +0000

    Favicon change, content changes

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -3,7 +3,7 @@ export default function Page() {
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is where your work experience goes.
+        This is where my work experience will go.
       </p>
     </section>
   );
