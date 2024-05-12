3607be310c5542b8ea4b591ee8ee9eaaf28a413f
sainsw <s.ainsworth@me.com>
2024-05-12 14:20:27 +0000

    Tweak CV

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -59,6 +59,7 @@ function ExperienceCard({ name, dates, post, description }) {
           <p className="prose-sm text-neutral-900 dark:text-neutral-100">
             {dates}
           </p>
+          <br />
           <p className="prose-sm text-neutral-900 dark:text-neutral-100">
             {description}
           </p>
